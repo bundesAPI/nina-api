4408a04ddd7409e9d80ae1ac2c8958659191d6aa
obsti8383 <github@florian-probst.de>
2021-08-31 20:21:21 +0200

    Anpassung an den von NINA verwendeten /warnings/{id}.json endpoint

--- Beispielcode/Python/ZeigeUeberblickWarnmeldungenFuerGebietscode.py
+++ Beispielcode/Python/ZeigeUeberblickWarnmeldungenFuerGebietscode.py
@@ -5,30 +5,24 @@ import json
 # ab Stelle 6 nur Nullen verwenden!
 ninaBaseUrl = "https://warnung.bund.de/api31"
 gebietscodeAugsburg="097720000000"
-mowasBaseUrl = "https://warnung.bund.de/bbk.mowas/"
  
 # aktuelle Warnmeldungen abrufen nach Gebietscode
-response = requests.get(ninaBaseUrl+"/dashboard/"+gebietscodeAugsburg+".json")
-# TODO: hier pruefen, ob Abruf erfolgreich war
+response = requests.get(ninaBaseUrl+"/dashboard/"+gebietscodeAugsburg+".json") # TODO: hier pruefen, ob Abruf erfolgreich war
+
 # wenn Abruf erfolgreich war, erhalten wir ein JSON
 warnungen = response.json()
+
 # iteriere über alle Warnmeldungen
 for warnung in warnungen:
-    # komplette Warnmeldung ausgeben
-    # print(warnung)
     # Der Dashboard-Abruf enthält nur eine Kurzform der Warnmeldung
     # Deshalb rufen wir hier die Details ab:
-    ## Annahme: MOWAS Warnmeldung, TODO: Erweiterung um andere Quellen
-    # das Feld "id" fängt bei MOWAS Meldungen mit "mow." an.
-    # die für den folgenden Abruf nötige MOWAS ID fängt nach dem "." an. 
-    id = warnung["payload"]["id"].split(".")[1] 
-    # Detailmeldung abrufen (TODO: Fehlerhandling ergaenzen)
-    meldung = requests.get(mowasBaseUrl+id+".json").json()
-    # Das Info-Feld ist ein Array, es ist unklar, ob dies in Realitaet mehrere Elemente enthalten kann;
-    # diese Beispiel liest nur das erste Info Element aus:
-    meldung = meldung["info"][0]["event"]+": "+meldung["info"][0]["headline"]+" (Sent: "+meldung["sent"]+", Status: "+meldung["status"]+")"
-    print("- "+meldung)
+    id = warnung["payload"]["id"]
+    warningDetails = requests.get(ninaBaseUrl+"/warnings/"+id+".json").json() # TODO: Fehlerbehandlung ergaenzen
+    # headline und description Felder aus dem JSON fuer die Ausgabe zusammensetzen
+    meldungsText = warningDetails["info"][0]["headline"]+ ": "+warningDetails["info"][0]["description"]
+    print("- "+meldungsText)
 
 ### Beispiel-Output:
-# - Gefahreninformation: Coronavirus: Informationen des Bundesministeriums für Gesundheit (Sent: 2020-10-14T16:35:21+02:00, Status: Actual)
-# - Gefahreninformation: Vorübergehende Änderung der Trinkwasserqualität: Chlorung besteht weiterhin (Sent: 2020-08-28T11:00:08+02:00, Status: Actual)
+# - Coronavirus: Informationen des Bundesministeriums für Gesundheit: Die Zahl der mit dem Corona-Virus infizierten Menschen steigt gegenwärtig stark an. Es wächst daher die Gefahr einer weiteren Verbreitung der Infektion und - je nach Einzelfall - auch von schweren Erkrankungen.
+# - Vorübergehende Änderung der Trinkwasserqualität: Chlorung besteht weiterhin: Die Chlorung besteht weiterhin.
+# - Hochwasserinformation Bayern: Die Hochwasserwellen an Iller, Donau, Isar und Inn laufen ab. In deren Einzugsgebieten befinden sich noch einzelne Pegel in Meldestufe 1 und 2. ::: Es liegen Warnungen der Wasserwirtschaftsämter vor.
