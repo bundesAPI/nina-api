19286f0af7d6ace549ac73a69bcfa443de73bbd5
bundesbot <bundesAPI@users.noreply.github.com>
2023-03-03 15:12:24 +0000

    Generate newest code from openapi.yaml

--- python-client/deutschland/nina/__init__.py
+++ python-client/deutschland/nina/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 # import ApiClient
 from deutschland.nina.api_client import ApiClient
--- python-client/deutschland/nina/api_client.py
+++ python-client/deutschland/nina/api_client.py
@@ -82,7 +82,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = "OpenAPI-Generator/1.0.2/python"
+        self.user_agent = "OpenAPI-Generator/1.1.0/python"
 
     def __enter__(self):
         return self
